77d68475f9ac33d7be55e54038d56cf245ce13db
Manuel404-dev <manuelviotor@gmail.com>
2023-05-31 12:48:15 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let text = document.getElementById('text')
+let bg = document.getElementById('body')
+
+if(bg.style.background === 'white'){
+    text.style.color = 'black'
+    text.innerText = text.style.color
+
+}else{
+    text.style.color = 'white'
+    text.innerText = text.style.color
+
+}
