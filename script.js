a34826c8c028f6df4586cc30c4bbc971c6b41ed9
Manuel <manuelviotor@gmail.com>
2024-07-28 16:30:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let text = document.getElementById('text')
 let bg = document.getElementById('body')
 
+bg.style.color = white
+text.style.color = black
+
 if(bg.style.background === 'white'){
     text.style.color = 'black'
     text.innerText = text.style.color
